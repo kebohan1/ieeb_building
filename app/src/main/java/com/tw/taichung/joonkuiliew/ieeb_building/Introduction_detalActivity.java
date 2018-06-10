e48711805476cf3f5ec092f0954e217f702c80a8
Collin(Lin Pin Yu) <qaz1232009@gmail.com>
2018-06-10 16:36:42 +0800

    新增Introduction_detalActivity 面頁建置的程式碼

--- app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/Introduction_detalActivity.java
+++ app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/Introduction_detalActivity.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Introduction_detalActivity extends AppCompatActivity {
     private ImageView imageView_detal;
+    private TextView introduction_detail_textView;
+    private ArrayList<Introductphoto> introductphotolist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,14 @@ public class Introduction_detalActivity extends AppCompatActivity {
         Intent intent = new Intent();
         int now_num = intent.getIntExtra("now_num",0);
 
+        //var declare
+        imageView_detal = findViewById(R.id.imageView_detal);
+        introduction_detail_textView = findViewById(R.id.introduction_detail_textView);
+        introductphotolist = ScrollingintroductionphotoActivity.introductphotolist;
+        Introductphoto now = introductphotolist.get(now_num);
+
         //set image and title text
+        introduction_detail_textView.setText(now.title);
+        imageView_detal.setImageResource(now.imgid);
     }
 }
--- app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/ScrollingintroductionphotoActivity.java
+++ app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/ScrollingintroductionphotoActivity.java
@@ -13,20 +13,13 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import java.util.ArrayList;
 
 public class ScrollingintroductionphotoActivity extends AppCompatActivity {
-    private ArrayList<Introductphoto> introductphotolist = new ArrayList<Introductphoto>();
+    static ArrayList<Introductphoto> introductphotolist = new ArrayList<Introductphoto>();
     private IntroductphotoArrayAdapter aboutphotoadapter;
     private ListView ls_photo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +30,7 @@ public class ScrollingintroductionphotoActivity extends AppCompatActivity {
         introductphotolist.add(test);
 
         //listView建置
+        aboutphotoadapter = new IntroductphotoArrayAdapter(this,introductphotolist);
         ls_photo = (ListView)findViewById(R.id.listView_about_photo);
         ls_photo.setAdapter(aboutphotoadapter);
 
