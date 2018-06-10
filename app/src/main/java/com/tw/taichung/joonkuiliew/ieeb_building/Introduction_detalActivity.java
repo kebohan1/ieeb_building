660fdc378414df8aa6b8581fa83c3c8b71a39ed2
Collin(Lin Pin Yu) <qaz1232009@gmail.com>
2018-06-10 16:59:14 +0800

    新增Introduction_detalActivity 夜間模式切換的程式碼

--- app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/Introduction_detalActivity.java
+++ app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/Introduction_detalActivity.java
@@ -3,8 +3,12 @@ package com.tw.taichung.joonkuiliew.ieeb_building;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -12,6 +16,9 @@ public class Introduction_detalActivity extends AppCompatActivity {
     private ImageView imageView_detal;
     private TextView introduction_detail_textView;
     private ArrayList<Introductphoto> introductphotolist;
+    private Switch nightmode_switch;
+    private int now_num = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +26,35 @@ public class Introduction_detalActivity extends AppCompatActivity {
 
         //Catch item num
         Intent intent = new Intent();
-        int now_num = intent.getIntExtra("now_num",0);
+        now_num = intent.getIntExtra("now_num", 0);
 
         //var declare
         imageView_detal = findViewById(R.id.imageView_detal);
         introduction_detail_textView = findViewById(R.id.introduction_detail_textView);
+        nightmode_switch = findViewById(R.id.nightmode_switch);
         introductphotolist = ScrollingintroductionphotoActivity.introductphotolist;
         Introductphoto now = introductphotolist.get(now_num);
 
         //set image and title text
         introduction_detail_textView.setText(now.title);
         imageView_detal.setImageResource(now.imgid);
+
+        //set nightmode_switch listener
+        nightmode_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Introductphoto now = introductphotolist.get(now_num);
+
+                if(compoundButton.isChecked()){
+                    Log.v("Switch_test","Open >_<");
+                    Toast.makeText(Introduction_detalActivity.this,"Open Night Mode",Toast.LENGTH_SHORT).show();
+                    imageView_detal.setImageResource(now.imgid_night);
+                } else{
+                    Log.v("Switch_test","Close!!!!");
+                    Toast.makeText(Introduction_detalActivity.this,"Close Night Mode",Toast.LENGTH_SHORT).show();
+                    imageView_detal.setImageResource(now.imgid);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/Introductphoto.java
+++ app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/Introductphoto.java
@@ -3,10 +3,26 @@ package com.tw.taichung.joonkuiliew.ieeb_building;
 public class Introductphoto {
     String title;
     int imgid;
+    int imgid_night;
 
     public Introductphoto(String title, int imgid) {
         this.title = title;
         this.imgid = imgid;
+        this.imgid_night = imgid;
+    }
+
+    public Introductphoto(String title, int imgid, int imgid_night) {
+        this.title = title;
+        this.imgid = imgid;
+        this.imgid_night = imgid_night;
+    }
+
+    public int getImgid_night() {
+        return imgid_night;
+    }
+
+    public void setImgid_night(int imgid_night) {
+        this.imgid_night = imgid_night;
     }
 
     public String getTitle() {
