048e9ad4aaaf13b0fcef5bb7ffeb2b37a3a18d6d
Collin(Lin Pin Yu) <qaz1232009@gmail.com>
2018-06-10 15:03:33 +0800

    新增floor_tourActivity判斷按鈕選取的樓層和設定文字圖片的method

--- app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/Floor_tourActivity.java
+++ app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/Floor_tourActivity.java
@@ -1,14 +1,79 @@
 package com.tw.taichung.joonkuiliew.ieeb_building;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Floor_tourActivity extends AppCompatActivity {
+    private ImageView floor_imageView;
+    private ImageView map_imageView;
+    private TextView floor_name_textView; //樓層
+    private TextView floordetile_textView; //主要系所
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floor_tour);
 
+        //var declare
+        floor_imageView = findViewById(R.id.floor_imageView);
+        map_imageView = findViewById(R.id.map_imageView);
+        floor_name_textView = findViewById(R.id.floor_name_textView);
+        floordetile_textView = findViewById(R.id.floordetile_textView);
+
+        //get choice floor
+        Intent intent = new Intent();
+        int now_floor = intent.getIntExtra("floor_key",0);
+
+        //select action
+        switch (now_floor){
+            case -1:
+                //還沒有圖片 底下那行為圖片設置程式碼
+                //floor_imageView.setImageResource(R.id.B1);
+                //map_imageView.setImageResource(R.id.B1_map);
+                floor_name_textView.setText("資電館B1");
+                floordetile_textView.setText("主要系所:{}{}學系");
+                break;
+            case 1:
+                //還沒有圖片 底下那行為圖片設置程式碼
+                //floor_imageView.setImageResource(R.id.first);
+                //map_imageView.setImageResource(R.id.first_map);
+                floor_name_textView.setText("資電館1F");
+                floordetile_textView.setText("主要系所:..學系");
+                break;
+            case 2:
+                //還沒有圖片 底下那行為圖片設置程式碼
+                //floor_imageView.setImageResource(R.id.second);
+                //map_imageView.setImageResource(R.id.second_map);
+                floor_name_textView.setText("資電館2F");
+                floordetile_textView.setText("主要系所:資訊工程學系");
+                break;
+            case 3:
+                //還沒有圖片 底下那行為圖片設置程式碼
+                //floor_imageView.setImageResource(R.id.thr);
+                //map_imageView.setImageResource(R.id.thr_map);
+                floor_name_textView.setText("資電館3F");
+                floordetile_textView.setText("主要系所:__ __學系");
+                break;
+            case 4:
+                //還沒有圖片 底下那行為圖片設置程式碼
+                //floor_imageView.setImageResource(R.id.fourth);
+                //map_imageView.setImageResource(R.id.fourth_map);
+                floor_name_textView.setText("資電館4F");
+                floordetile_textView.setText("主要系所:XX學系");
+                break;
+            case 5:
+                //還沒有圖片 底下那行為圖片設置程式碼
+                //floor_imageView.setImageResource(R.id.fifth);
+                //map_imageView.setImageResource(R.id.fifth_map);
+                floor_name_textView.setText("資電館5F");
+                floordetile_textView.setText("主要系所:??學系");
+                break;
+            default:
+                floor_name_textView.setText("BUG");
+                floordetile_textView.setText("Error Error");
+        }
     }
 }
--- app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/tourActivity.java
+++ app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/tourActivity.java
@@ -1,13 +1,101 @@
 package com.tw.taichung.joonkuiliew.ieeb_building;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class TourActivity extends AppCompatActivity {
+    private ImageButton floor_imageButtonB1;
+    private ImageButton floor_imageButton1;
+    private ImageButton floor_imageButton2;
+    private ImageButton floor_imageButton3;
+    private ImageButton floor_imageButton4;
+    private ImageButton floor_imageButton5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour);
+
+        floor_imageButtonB1 = findViewById(R.id.floor_imageButtonB1);
+        //還沒有圖片 底下那行為圖片設置程式碼
+        // floor_imageButtonB1.setImageResource(R.drawable.firefox);
+        floor_imageButtonB1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(TourActivity.this,Floor_tourActivity.class);
+                intent.putExtra("floor_key",-1);
+                startActivity(intent);
+            }
+        });
+
+        floor_imageButton1 = findViewById(R.id.floor_imageButton1);
+        //還沒有圖片 底下那行為圖片設置程式碼
+       // floor_imageButton1.setImageResource(R.drawable.firefox);
+        floor_imageButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(TourActivity.this,Floor_tourActivity.class);
+                intent.putExtra("floor_key",1);
+                startActivity(intent);
+            }
+        });
+
+        floor_imageButton2 = findViewById(R.id.floor_imageButton2);
+        //還沒有圖片 底下那行為圖片設置程式碼
+        // floor_imageButton2.setImageResource(R.drawable.firefox);
+        floor_imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(TourActivity.this,Floor_tourActivity.class);
+                intent.putExtra("floor_key",2);
+                startActivity(intent);
+            }
+        });
+
+        floor_imageButton3 = findViewById(R.id.floor_imageButton3);
+        //還沒有圖片 底下那行為圖片設置程式碼
+        // floor_imageButton3.setImageResource(R.drawable.firefox);
+        floor_imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(TourActivity.this,Floor_tourActivity.class);
+                intent.putExtra("floor_key",3);
+                startActivity(intent);
+            }
+        });
+
+        floor_imageButton4 = findViewById(R.id.floor_imageButton4);
+        //還沒有圖片 底下那行為圖片設置程式碼
+        // floor_imageButton4.setImageResource(R.drawable.firefox);
+        floor_imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(TourActivity.this,Floor_tourActivity.class);
+                intent.putExtra("floor_key",4);
+                startActivity(intent);
+            }
+        });
+
+        floor_imageButton5 = findViewById(R.id.floor_imageButton5);
+        //還沒有圖片 底下那行為圖片設置程式碼
+        // floor_imageButton5.setImageResource(R.drawable.firefox);
+        floor_imageButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(TourActivity.this,Floor_tourActivity.class);
+                intent.putExtra("floor_key",5);
+                startActivity(intent);
+            }
+        });
     }
 }
