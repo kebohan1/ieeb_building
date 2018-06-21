92d5afbcf129ae337cdb43fceed47291e4e2f11a
kbh <kebohan1@gmail.com>
2018-06-21 09:59:31 +0800

    Merge branch 'master' of https://github.com/kebohan1/ieeb_building
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/build.gradle
    新增mailto，新增分享，新增撥打電話

--- app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/TeacherActivity.java
+++ app/src/main/java/com/tw/taichung/joonkuiliew/ieeb_building/TeacherActivity.java
@@ -3,6 +3,7 @@ package com.tw.taichung.joonkuiliew.ieeb_building;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -28,6 +30,9 @@ public class TeacherActivity extends AppCompatActivity {
     private TextView teacher_email;
     private TextView teacher_tel;
     private TextView teacher_office;
+    private ImageButton ib_share;
+    private ImageButton ib_mail;
+    private ImageButton ib_phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,12 +44,12 @@ public class TeacherActivity extends AppCompatActivity {
         Teacher teacher = null;
         Bundle bundle = intent.getExtras();
 
-        String name = bundle.getString("name");
+        final String name = bundle.getString("name");
         String department = bundle.getString("department");
-        String email = bundle.getString("email");
-        String jobs = bundle.getString("jobs");
-        String tel = bundle.getString("tel");
-        String office = bundle.getString("office");
+        final String email = bundle.getString("email");
+        final String jobs = bundle.getString("jobs");
+        final String tel = bundle.getString("tel");
+        final String office = bundle.getString("office");
         Bitmap img = bundle.getParcelable("bitmap");
         String[] skills_s = bundle.getStringArray("skills");
         ArrayList<String> skills = new ArrayList<>();
@@ -61,6 +66,37 @@ public class TeacherActivity extends AppCompatActivity {
         teacher_tel.setText(tel);
         teacher_office = (TextView) findViewById(R.id.tv_office);
         teacher_office.setText(office);
+        ib_mail = (ImageButton) findViewById(R.id.ib_mail);
+        ib_mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mail_intent = new Intent();
+                mail_intent.setAction(Intent.ACTION_SENDTO);
+                mail_intent.setData(Uri.parse("mailto:"+email));
+                startActivity(mail_intent);
+            }
+        });
+        ib_share = (ImageButton) findViewById(R.id.ib_share);
+        ib_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent share_intent = new Intent();
+                share_intent.setAction(Intent.ACTION_SEND);
+                share_intent.setType("text/plain");
+                share_intent.putExtra(Intent.EXTRA_TEXT,"我想跟你分享一位老師："+name+",他的辦公室在："+office+",辦公室分機為："+tel+",有機會我們可以一起去找找看老師～【由資訊電機館app自動分享】");
+                startActivity(Intent.createChooser(share_intent,"分享"+name+"至"));
+            }
+        });
+        ib_phone = (ImageButton) findViewById(R.id.ib_phone);
+        ib_phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent phone_intent = new Intent();
+                phone_intent.setAction(Intent.ACTION_CALL);
+                phone_intent.setData(Uri.parse("tel:0424517250"+tel));
+                startActivity(phone_intent);
+            }
+        });
         for(int i=0;i<skills_s.length;i++)
         {
             if(skills_s[i]!=null)
